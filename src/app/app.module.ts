37d7e2a255e1b5f55aa265e49de470d4e82795d8
ElKoloWirz96 <agustin-96@live.com.ar>
2024-03-20 18:16:09 -0300

    desafio terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentsPageComponent } from './students-page/students-page.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    StudentsPageComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/students-page/students-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-page',
+  templateUrl: './students-page.component.html',
+  styleUrl: './students-page.component.scss'
+})
+export class StudentsPageComponent {
+
+}
